729bbddf484f3607a536dffeb537389ad9512d7b
sleekEagle <lahirunuwanisme@gmail.com>
2022-11-03 10:04:21 -0400

    connect to RDS and local db from all functions

--- controller.py
+++ controller.py
@@ -24,8 +24,6 @@ import sched
 
 s = sched.scheduler(time.time, time.sleep)
 
-
-
 from importlib import reload
 reload(logging)
 
@@ -51,28 +49,33 @@ except:
 #how frequent we upload data (in seconds)
 file_freq=3.17*60*60
 def upload_files(sc):
+    rds_connection=rds.RDS() 
+    local_connection=rds.Local()
     try:
-        reload(logging)
-        logging.basicConfig(level = logging.INFO, 
-                        filename =Log.get_log_path(),
-                        format = '%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)s - %(funcName)20s()] %(message)s')
-        
-        sleep_time=file_freq
-        print('in upload_files()')
-        logging.info('starting the upload_files() function')
-        try:
-            ts_start=time.time()
+        if(isinstance(rds_connection,rds.RDS) and isinstance(local_connection,rds.Local)):
+            reload(logging)
+            logging.basicConfig(level = logging.INFO, 
+                            filename =Log.get_log_path(),
+                            format = '%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)s - %(funcName)20s()] %(message)s')
             
-            s3_upload.upload_files()
-            
-            ts_end=time.time()
-            #elapsed time in seconds
-            elapsed=(ts_end-ts_start)
-            sleep_time=file_freq-elapsed
-        except Exception as e:
-            print('Exception in controller ' + str(e))
-            logging.error('Exception in controller upload_files() function'+str(e))
-        sc.enter(file_freq, 1, upload_files, (sc,))
+            sleep_time=file_freq
+            print('in upload_files()')
+            logging.info('starting the upload_files() function')
+            try:
+                ts_start=time.time()
+                
+                s3_upload.upload_files()
+                
+                ts_end=time.time()
+                #elapsed time in seconds
+                elapsed=(ts_end-ts_start)
+                sleep_time=file_freq-elapsed
+            except Exception as e:
+                print('Exception in controller ' + str(e))
+                logging.error('Exception in controller upload_files() function'+str(e))
+            sc.enter(file_freq, 1, upload_files, (sc,))
+        else:
+            logging.error('Exception in controller upload_files() function. Cannot obtain local/cloud db connection '+str(e))
     except Exception as e:
         logging.error("Exception in controller upload_files() function "+str(e))
     finally:
@@ -92,7 +95,6 @@ def upload_db(sc):
         logging.info('starting the upload_db() function')
         
         print('uploading data to RDS db...')
-        sleep_time=upload_freq
         rds_connection=rds.RDS() 
         local_connection=rds.Local()
         try:
@@ -141,13 +143,8 @@ def upload_db(sc):
                 except Exception as e:
                     print('Exception in controller ' + str(e))
                 '''   
-                
-                
-            
-                ts_end=time.time()
-                #elapsed time in seconds
-                elapsed=(ts_end-ts_start)
-                sleep_time=upload_freq-elapsed
+            else:
+                logging.error("Exception in controller upload_db() function cannot obtain local/cloud db connection  "+str(e))
         except Exception as e:
             print('Exception in controller ' + str(e))
             logging.error("in controller uploading data to RDS "+str(e))
@@ -159,26 +156,24 @@ def upload_db(sc):
 #frequency to create missing data reports in seconds
 missing_data_freq=3*60*60
 def log_missing_data(sc):
-    
+    rds_connection=rds.RDS() 
+    local_connection=rds.Local()
     try:
-        reload(logging)
-        logging.basicConfig(level = logging.INFO, 
-                    filename =Log.get_log_path(),
-                    format = '%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)s - %(funcName)20s()] %(message)s')
-    
-        ts_start=time.time()
-        dep_id=dep_data.get_dep_id(file_system_tasks.get_project_dir(-3))
-        print(dep_id)
-        print('creating ema_storing_data and reward_data missing logs...')
-        logging.info('creating ema_storing_data and reward_data missing logs...')
-        missing_data.insert_missing_data(rds_connection,local_connection,'reward_data','missing_reward_data','TimeSent')
-        missing_data.insert_missing_data(rds_connection,local_connection,'ema_storing_data','missing_ema_storing_data','time')
-        print('done uploading missing data rows')
-       
-        ts_end=time.time()
-        #elapsed time in seconds
-        elapsed=(ts_end-ts_start)
-        sleep_time=missing_data_freq-elapsed  
+        if(isinstance(rds_connection,rds.RDS) and isinstance(local_connection,rds.Local)):
+            reload(logging)
+            logging.basicConfig(level = logging.INFO, 
+                        filename =Log.get_log_path(),
+                        format = '%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)s - %(funcName)20s()] %(message)s')
+        
+            dep_id=dep_data.get_dep_id(file_system_tasks.get_project_dir(-3))
+            print(dep_id)
+            print('creating ema_storing_data and reward_data missing logs...')
+            logging.info('creating ema_storing_data and reward_data missing logs...')
+            missing_data.insert_missing_data(rds_connection,local_connection,'reward_data','missing_reward_data','TimeSent')
+            missing_data.insert_missing_data(rds_connection,local_connection,'ema_storing_data','missing_ema_storing_data','time')
+            print('done uploading missing data rows')
+        else:
+            logging.error("Exception in controller log_missing_data() function cannot obtain local/cloud db connection  "+str(e))
     except Exception as e:
         logging.error("Exception in log_missing_data "+str(e))
     finally:
--- heart_beat.py
+++ heart_beat.py
@@ -21,6 +21,7 @@ col_names='dep_id,ts'
 def insert_heart_beat(rds_connection):        
     try:
         dep_id=dep_data.get_dep_id(file_system_tasks.get_project_dir(-3))
+        print(dep_id)
         ts=time.time()
         values=str(dep_id)+','+str(int(ts))
         res=rds_connection.insert_row(table_name,col_names,values)   
